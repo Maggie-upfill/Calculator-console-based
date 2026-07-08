d6936264718f87b9190c97ca6b36d9b14685ab2c
Maggie <mhwanjiro@gmail.com>
2026-07-08 19:17:59 +0300

    Outside class defined all helper functions

--- Calculator-console-based.cpp
+++ Calculator-console-based.cpp
@@ -24,6 +24,44 @@ void clearInput()
 	std::cin.clear();
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
+}
+
+//Get two valid integers from the user
+void Calculator::getNumbers()
+{
+	while (true)
+	{
+		std::cout << "Enter two numbers: ";
+
+		if (std::cin >> a >> b)
+		{
+			return; // valid input, exit function
+		}
+
+		std::cout << "Invalid input. Please enter numbers only.\n";
+
+		clearInput();
+	}
+
+}
+
+//Helper function to print the result
+void Calculator::printResult(double result)
+{
+	std::cout << "Result: " << result << std::endl;
+}
+
+//Helper function to check if division or modulo is valid
+bool Calculator::isDivisionValid()
+{
+	if (b == 0)
+	{
+		std::cout << "Error: Division by zero!\n";
+		return false;
+	}
+
+	return true;
+}
 
 int main()
 {
